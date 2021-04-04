987b6efb15d5bb022276ab92a57f9f853d63024f
ahmadTAhas <ahmad.taha.sartawii@gmail.com>
2021-04-04 14:28:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-yaxjut
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-yaxjut)
---
+++ index.ts
@@ -0,0 +1,42 @@
+/*let mynum , mynum2 : number , oprator : string ;
+mynum = parseInt(prompt("please enter a number"));
+oprator = prompt("please enter a oprator");
+mynum2 = parseInt(prompt("please enter a number"));
+
+main (mynum , mynum2 , oprator);
+
+function plus(num1 : number , num2 : number ){
+
+return num1 + num2;
+
+}
+function mainus(num1 : number , num2 : number ){
+
+return num1 - num2;
+
+}
+function darb(num1 : number , num2 : number ){
+
+return num1 * num2;
+
+}
+function qismah(num1 : number , num2 : number ){
+
+return num1 / num2;
+
+}
+function main (num1 , num2 : number , strin : string){
+  let result : number;
+  if (strin == "+"){
+    result = plus(num1 , num2);
+  }else if (strin == "-"){
+    
+    result = mainus( num1 ,num2);
+  }else if(strin == "*"){
+    result = darb(num1 , num2);
+  }else if (strin == "/"){
+    result = qismah(num1 ,num2);
+  }
+  console.log(result);
+}
+*/
